5280e2bb748f13eeeebd8e7ce984ce929012d63d
Fleur Kelpin <fleur@kelpin.nl>
2013-12-19 02:22:39 +0100

    import

---
+++ webapp/src/test/java/nl/umcg/webapp/ApplicationTests.java
@@ -0,0 +1,19 @@
+package nl.umcg.webapp;
+
+import nl.umcg.webapp.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class, loader=SpringApplicationContextLoader.class)
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
